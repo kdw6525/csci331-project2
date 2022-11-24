e94f603ac2c9ebd3006ac26d24f72899d3ee5f58
kdw6525 <kdw6525@rit.edu>
2022-11-24 11:56:11 -0500

    Graphing path function is finished fitness calculations implemented

--- autonomous_parking.py
+++ autonomous_parking.py
@@ -2,6 +2,7 @@
 #
 # This program will perform a genetic learning algorithm using reinforcement learning.
 #
+import math
 
 import numpy as np
 import scipy as sp
@@ -36,9 +37,12 @@ STEP_DELTA = 0.1
 # Regions the agents cannot touch or pass into
 X_BEFORE_SPOT = -4
 Y_BEFORE_SPOT = 3
+Y_BEFORE_SPOT_SQ = Y_BEFORE_SPOT ** 2
 Y_SPOT = -1
+Y_SPOT_SQ = -1 ** 2
 X_AFTER_SPOT = 4
 Y_AFTER_SPOT = 3
+Y_AFTER_SPOT_SQ = 3 ** 2
 
 # START and TARGET defined by the project
 # [x, y, angle, velocity]
@@ -57,8 +61,8 @@ def interpolate_individual(individual):
     convert_range = np.vectorize(convert_to_range)
 
     t = range(0, 10)
-    gamma = convert_range(individual[0::2], GAMMA_LB, GAMMA_UB)
-    beta = convert_range(individual[1::2], BETA_LB, BETA_UB)
+    gamma = convert_range(individual[0::2], GAMMA_LB, GAMMA_UB - GAMMA_LB)
+    beta = convert_range(individual[1::2], BETA_LB, BETA_UB - BETA_LB)
 
     # interpolate using cubic spline
     f_gamma = sp.interpolate.CubicSpline(t, gamma, bc_type='natural')
@@ -69,17 +73,49 @@ def interpolate_individual(individual):
     gamma_new = f_gamma(t_new)
     beta_new = f_beta(t_new)
 
-    return gamma_new, beta_new
+    return gamma_new, beta_new, t_new
 
 
 def calculate_step(step, gamma, beta):
     # calculates the next step
     x, y, a, v = step
-    x_new = 0
-    return
+    x_new = x + (v * math.cos(a) * STEP_DELTA)
+    y_new = y + (v * math.sin(a) * STEP_DELTA)
+    a_new = a + (gamma * STEP_DELTA)
+    v_new = v + (beta * STEP_DELTA)
+    return np.array([x_new, y_new, a_new, v_new])
 
 
-def euler_method(population_steps):
+def individual_fitness(current, end, gamma_beta):
+    # calculates the fitness using euler's method
+    gamma = gamma_beta[0]
+    beta = gamma_beta[1]
+
+    infeasibility = 0
+    for g, b in zip(gamma, beta):
+        current = calculate_step(current, g, b)
+        # check if feasible spot and add infeasibility costs
+        infeasibility += calc_infeasibility(current)
+
+    dist = scipy.spatial.distance.euclidean(current, end)
+    return dist + infeasibility
+
+
+def individual_history(current, gamma_beta):
+    # generates the history of an individual using euler's method
+    gamma = gamma_beta[0]
+    beta = gamma_beta[1]
+
+    history = np.array([current])
+    for g, b in zip(gamma, beta):
+        current = calculate_step(current, g, b)
+        # record current in history
+        history = np.vstack((history, current))
+
+    return history
+
+
+def calculate_fitness(population_steps):
     # performs the euler method STEPS times
     start = np.copy(START)
 
@@ -94,9 +130,30 @@ def choose_parents(population, labels, probabilities, random_generator: np.rando
     return np.array([population[parents[0]], population[parents[1]]])
 
 
-def convert_to_range(val, lb, ub):
+def evaluate_best_individual(individual):
+    # perform analysis on the best fit individual
+    # analysis ranges from exporting control variables to graphing various properties
+    # TODO add exporting and additional graphs. Perhaps save the graphs as pdfs
+
+    gamma_new, beta_new, t_new = interpolate_individual(individual)
+
+    history = individual_history(np.copy(START), [gamma_new, beta_new])
+    # fist column is x histories
+    x_new = history[:, 0]
+    # second column is y histories
+    y_new = history[:, 1]
+
+    # the path taken is related to x and y
+    plt.figure(figsize=(10, 8))
+    plt.plot(x_new, y_new, 'b')
+    plt.title('Path')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+
+def convert_to_range(val, lb, r):
     # converts val to a value between lb and ub.
-    r = ub - lb
     return (val / MAX_GENE_VAL) * r + lb
 
 
@@ -116,13 +173,18 @@ def gray_decode(n):
     return n
 
 
-def is_feasible(current):
+def calc_infeasibility(current):
     # Checks if the current step is in the feasible region.
     x = current[0]
     y = current[1]
-    return ((x <= X_BEFORE_SPOT and y > Y_BEFORE_SPOT) or
-            (X_BEFORE_SPOT < x < X_AFTER_SPOT and y > Y_SPOT) or
-            (x >= X_AFTER_SPOT and y > Y_AFTER_SPOT))
+    if x <= X_BEFORE_SPOT and y <= Y_BEFORE_SPOT:
+        return INFEASIBILITY + (Y_BEFORE_SPOT_SQ - (y ** 2))
+    elif X_BEFORE_SPOT < x < X_AFTER_SPOT and y <= Y_SPOT:
+        return INFEASIBILITY + (Y_SPOT_SQ - (y ** 2))
+    elif x >= X_AFTER_SPOT and y <= Y_AFTER_SPOT:
+        return INFEASIBILITY + (Y_AFTER_SPOT_SQ - (y ** 2))
+    else:
+        return 0
 
 
 def main(args):
@@ -137,6 +199,7 @@ def main(args):
     population_steps = np.apply_along_axis(interpolate_individual, 1, population)
 
     print(population_steps.shape)
+    evaluate_best_individual(population[0])
 
     # probs = scipy.special.softmax(labels)
     # parents = choose_parents(population, labels, probs, random_generator)
